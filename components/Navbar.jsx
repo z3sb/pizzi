a359d64595d7331ab16769d06566063ff65d96da
z3sb <oslvuao@gmail.com>
2023-01-21 13:23:34 +0300

    Create Next App and start with index route

---
+++ components/Navbar.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image"
+import styles from "../styles/Navbar.module.css"
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image src="/img/telephone.png" alt="" width="32" height="32" />
+        </div>
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDER NOW!</div>
+          <div className={styles.text}>07727599259</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <h2 className={styles.logo}>PIZZI</h2>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="" width="30" height="30" />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
